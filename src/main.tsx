6ff297d1717bf41c7a1cba18d28817a199fc196c
WonbinLee <wonbin145@gmail.com>
2024-09-24 18:39:44 +0900

    update: 컨펌 알림 커스터마이징을 추가했습니다.

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,9 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import toast from "./utils/toast";
 
 createRoot(document.getElementById("root")!).render(
   <>
     <App />
   </>
 );
-
-
-
-toast.confirm("출력메세지", {
-  cancleBtn: "취소 버튼 문구"
-});
