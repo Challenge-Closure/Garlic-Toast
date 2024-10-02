5445f552d499c61ee1fc591d9091fcaea78caedc
PJH-FE <sjrmd789@gmail.com>
2024-10-02 19:46:21 +0900

    fix: 정상적으로 복사가 안 되어 소스 수정

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-cd ../ || exit 1  # 상위 디렉토리로 이동, 실패 시 종료
-mkdir -p output    # output 디렉토리 생성, 이미 존재하면 무시
-find ./Garlic-Toast/ -type f ! -name 'vite.config.ts' -exec cp {} ./output/ \;  # 파일 복사
-cp -R ./output/* ./Garlic-Toast/  # output의 내용만 복사
+cd ../
+mkdir output
+cp -R ./Garlic-Toast/* ./output
+rm -f output/vite.config.ts
+cp -R ./output ./Garlic-Toast/
