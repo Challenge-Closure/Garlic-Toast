526be8b4e9aa7c75d2c30671647e946c1ea585d0
PJH-FE <sjrmd789@gmail.com>
2024-10-02 19:20:44 +0900

    fix: build.sh 수정

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-cd ../garlic-demo || exit 1  # garlic-demo 디렉토리로 이동
-mkdir -p output                # output 디렉토리 생성
-find ../Garlic-Toast -type f ! -name 'vite.config.ts' -exec cp {} ./output/ \;  # Garlic-Toast에서 파일 복사
+cd ../
+mkdir output
+find ./Garlic-Toast/ -type f ! -name 'vite.config.ts' -exec cp {} ./output
+cp -R ./output ./Garlic-Toast/
